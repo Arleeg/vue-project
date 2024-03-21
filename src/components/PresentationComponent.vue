0c0f43df4286b9e392761ca99dd7b4e81ffb67c8
AleDiBene <socialorne02@gmail.com>
2024-03-21 16:46:31 +0100

    change img

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -2,9 +2,13 @@
 
 <template>
   <section class="grid grid-cols-2 gap-4">
-    <div class="bg-white-200 p-4"></div>
-    <div class="bg-white-200 p-4">
-        <img class="rotate-90" src="/Users/alessandrodibenedetto/Desktop/SJ/Vue/vue-project/src/img/IMG_0904 senza sfondo.png" alt="Testo alternativo dell'immagine">
+    <div class="bg-white-200"></div>
+    <div class="bg-white-200">
+      <img
+        class="rotate-90 h-64"
+        src="/Users/alessandrodibenedetto/Desktop/SJ/Vue/vue-project/src/img/pic.png"
+        alt="Testo alternativo dell'immagine"
+      />
     </div>
   </section>
 </template>
