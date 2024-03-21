9d3d13843f25fe7cf55de21356ef70b2e086c427
Francesco sortino <francescosortino29@gmail.com>
2024-03-21 17:47:34 +0100

    Ultime modifiche

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -4,7 +4,7 @@
   <section class="grid grid-cols-2 gap-4">
     <div class="bg-white-200 p-4"></div>
     <div class="bg-white-200 p-4">
-        <img class="rotate-90" src="../img/IMG_0904 senza sfondo.png" alt="Testo alternativo dell'immagine">
+        <img class="rotate-90" src="../img/pic.png" alt="Testo alternativo dell'immagine">
 
     </div>
   </section>
