7532d50bbbdebca5f7b278cb72c36ffcbbb8db0e
AleDiBene <socialorne02@gmail.com>
2024-03-21 17:21:06 +0100

    Salva le modifiche locali prima di eseguire il merge

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -5,6 +5,7 @@
     <div class="bg-white-200 p-4"></div>
     <div class="bg-white-200 p-4">
         <img class="rotate-90" src="../img/IMG_0904 senza sfondo.png" alt="Testo alternativo dell'immagine">
+
     </div>
   </section>
 </template>
