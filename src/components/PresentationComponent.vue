336e0213197709093e42458cf54acd9d35ce80cb
AleDiBene <socialorne02@gmail.com>
2024-03-21 16:23:35 +0100

    secondo commit Ale

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -2,8 +2,10 @@
 
 <template>
   <section class="grid grid-cols-2 gap-4">
-    <div class="bg-gray-200 p-4">Contenuto della prima colonna</div>
-    <div class="bg-gray-200 p-4">Contenuto della seconda colonna</div>
+    <div class="bg-white-200 p-4"></div>
+    <div class="bg-white-200 p-4">
+        <img class="rotate-90" src="/Users/alessandrodibenedetto/Desktop/SJ/Vue/vue-project/src/img/IMG_0904 senza sfondo.png" alt="Testo alternativo dell'immagine">
+    </div>
   </section>
 </template>
 
