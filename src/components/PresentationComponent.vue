1b503cf143123efff92e4c50c0b6862ff7152e27
Francesco sortino <francescosortino29@gmail.com>
2024-03-21 17:58:22 +0100

    Descrizione del commit

--- src/components/PresentationComponent.vue
+++ src/components/PresentationComponent.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="grid grid-cols-2 gap-4">
-    <div class="bg-white-200 p-4"></div>
-    <div class="bg-white-200 p-4">
+
+    
+    <div class="bg-gray-200 p-4">
         <img class="rotate-90" src="../img/pic.png" alt="Testo alternativo dell'immagine">
 
     </div>
-  </section>
+  
 </template>
 
 <style scoped></style>
