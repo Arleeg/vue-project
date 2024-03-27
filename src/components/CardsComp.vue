39e64232560de0566c4a72ae43cbf7e28cab1184
Francesco sortino <francescosortino29@gmail.com>
2024-03-27 10:16:17 +0100

    CardComp new

--- src/components/CardsComp.vue
+++ src/components/CardsComp.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
- <div></div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" src= "https://www.instagram.com/p/C40I4g0IRMZ/" alt="Sunset in the mountains">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">Titolo della Card</div>
+      <p class="text-gray-700 text-base">
+        Contenuto della card. Qui puoi inserire la descrizione o il contenuto che preferisci.
+      </p>
+    </div>
+
+  </div>
+
 </template>
 
 <style scoped>
